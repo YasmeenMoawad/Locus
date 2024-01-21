3578b65b2cec125f46c11c546818422f2a6a2328
YasmeenMoawad <yasmeenmwd@gmail.com>
2024-01-21 03:09:32 +0300

    adding images and fonts

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 
 class LocusColors {
   static const Color primaryColor = Color(0xFF2D3380);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0x66EBEEFF);
+  static const Color lightGrey = Color(0xFFC4C4C4);
+  static const Color opacityGrey = Color(0x80000000);
+  static const Color inactiveIcon = Color(0xFF676D75);
 
 }
