e4ef2c8032e0f77b1c38fdf572038c3294d42cb3
YasmeenMoawad <yasmeenmwd@gmail.com>
2024-01-28 23:54:25 +0300

    Chat Bot & AI Generator UI Completed

--- lib/features/chatbot_and_AI_generator/presentation/views/AI_generator_view.dart
+++ lib/features/chatbot_and_AI_generator/presentation/views/AI_generator_view.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:locus/features/chatbot_and_AI_generator/presentation/views/widgets/chat_bar.dart';
+import 'package:locus/features/chatbot_and_AI_generator/presentation/views/widgets/locus_msgs.dart';
+import 'package:locus/features/chatbot_and_AI_generator/presentation/views/widgets/sender_msgs.dart';
 
 class AIGeneratorView extends StatelessWidget {
   const AIGeneratorView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'AI Generate',
-        style: TextStyle(
-            color: Colors.white
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(width: 1.w,),
+            SenderMsgs(),
+          ],
         ),
-      ),
+        SizedBox(height: 25.h,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LocusMsgs(),
+            SizedBox(width: 1.w,),
+          ],
+        ),
+        SizedBox(height: 15.h,),
+        ChatBar(),
+        SizedBox(height: 10.h,),
+      ],
     );
   }
 }
